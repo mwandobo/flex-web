fe4e439b68a52b6aa303d65378ec2acb63cf83b3
Boniface Mwandobo <breezojr@gmai.com>
2024-07-04 08:03:48 +0300

    monitoring refactor

--- src/app/project-management/project-monitoring/[monitoringId]/page.tsx
+++ src/app/project-management/project-monitoring/[monitoringId]/page.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 import LinearWithValueLabel from "@/components/bars/progressBar";
 import CircularWithValueLabel from "@/components/bars/circularBar";
 import { getValueFromLocalStorage, setValueLocalStorage } from "@/utils/actions/local-starage";
-import { Activity, ChevronDown, ChevronUp, CircleCheckBig, ClipboardCheck, OctagonX } from "lucide-react";
+import { Activity, BetweenHorizontalStart, ChevronDown, ChevronUp, CircleCheckBig, ClipboardCheck, OctagonX } from "lucide-react";
 import { useGlobalContextHook } from "@/hooks/useGlobalContextHook";
 import FormattedMoney from "@/components/moneyFormater";
 
@@ -18,6 +18,7 @@ const ProjectMonitoringShow = ({ params }: { params: { monitoringId: string } })
     const [loading, setLoading] = useState(false)
     const [isSubmitted, setIsSubmitted] = useState(false)
     const [isCollecting, setIsCollecting] = useState(false)
+    const [monitoredItemState, setMonitoredItemState] = useState<any>()
     const [formPayload, setFormPayload] = useState<any>()
     const token = getValueFromLocalStorage('token')
     const { state, dispatch } = useGlobalContextHook()
@@ -79,7 +80,6 @@ const ProjectMonitoringShow = ({ params }: { params: { monitoringId: string } })
                 setFormPayload({ ...formPayload, indicator_id: indicator.activity_id, cost: e.target.value, from: indicator.from, from_id: indicator.from_id }); break;
             default: break;
         }
-
     }
 
     const handleSubmitCollectedData = async () => {
@@ -88,11 +88,31 @@ const ProjectMonitoringShow = ({ params }: { params: { monitoringId: string } })
             if (response?.status === 200) {
                 setIsSubmitted(!isSubmitted)
                 setFormPayload(null)
+                setMonitoredItemState(null)
             } else {
                 console.log('errorHappened')
             }
         }
     }
+
+    const handleSetMonitoredItemState = (from: string, id: string) => {
+        setMonitoredItemState({ from, id })
+    }
+
+    const checkIsMonitoredItem = (from: string, id: string) => {
+        console.log(monitoredItemState)
+        console.log(from)
+        console.log(id)
+
+        let check = false
+        if (from === monitoredItemState?.from && id === monitoredItemState.id) {
+            check = true
+        }
+
+        return check
+    }
+
+    console.log(monitoredItemState)
 
     useEffect(() => {
         const fetchData = async () => {
@@ -130,14 +150,15 @@ const ProjectMonitoringShow = ({ params }: { params: { monitoringId: string } })
             <div className="w-11/12 mx-auto border border-gray-300 flex flex-col">
                 <div className="font-semibold text-sm py-2 ps-2"><h5>Indicators List</h5></div>
                 <div className="">
-                    <div className="grid grid-cols-7 text-xs border-b border-t border-gray-300">
+                    <div className="grid grid-cols-8 text-xs border-b border-t border-gray-300">
                         <p className="border-r border-gray-300 ps-2 py-2">#</p>
                         <p className="border-r border-gray-300 ps-2 py-2 ">Code</p>
                         <p className="border-r border-gray-300 ps-2  py-2">Indicator Name</p>
                         <p className="border-r border-gray-300 ps-2 py-2">Verify By</p>
                         <p className="border-r border-gray-300 ps-2 py-2">Target Data</p>
                         <p className="border-r border-gray-300 ps-2 py-2">Collected Data</p>
-                        <p className="ps-2 py-2">  {!isCollecting ? "Progress" : "Action"}</p>
+                        <p className="border-r border-gray-300 ps-2 py-2">Progress </p>
+                        <p className="ps-2 py-2">Action</p>
                         <p className=""></p>
                     </div>
                 </div>
@@ -145,32 +166,42 @@ const ProjectMonitoringShow = ({ params }: { params: { monitoringId: string } })
                     {
                         payload.map((item: any, index: any) =>
                             <div key={index} className="flex ">
-                                <div className="grid grid-cols-7 w-full text-xs border-b border-gray-300 ">
+                                <div className="grid grid-cols-8 w-full text-xs border-b border-gray-300 ">
                                     <p className="border-r border-gray-300 ps-3  py-1">{index + 1}</p>
                                     <p className="border-r border-gray-300 ps-3 py-1">{item.formatted_code}</p>
                                     <p className="border-r border-gray-300 ps-3 py-1">{item.name}</p>
                                     <p className="border-r border-gray-300 ps-3 py-1">{item.mov}</p>
                                     <p className="text-end border-r pe-3 border-gray-300 ps-2 py-1">{item.target_data}</p>
-                                    {isCollecting ?
+                                    <p className="text-end pe-3 border-r border-gray-300 ps-2 py-1">{item.collected_data}</p>
+
+                                    {checkIsMonitoredItem(item.from, item.id) ?
                                         <div className="ps-3 border-r border-gray-300 py-1">
                                             <input type="text" placeholder="Enter Data" className="ps-1 h-7 w-20" onChange={(e) => handleFormInputChange(e, item, 'progress')} />
                                         </div>
                                         :
-                                        <p className="text-end pe-3 border-r border-gray-300 ps-2 py-1">{item.collected_data}</p>
+                                        <p className="ps-3">{progresRender(item.progress)}</p>
                                     }
 
-                                    {isCollecting ?
+                                    {checkIsMonitoredItem(item.from, item.id) ?
                                         <div className="ps-3 py-1 border-r border-gray-300">
                                             <button
+                                                style={{ fontSize: '10px' }}
                                                 className={`h-7 w-16 flex justify-center items-center px-1 text-white border border-gray-300 bg-gray-500  shadow-md hover:shadow-lg transition-shadow duration-300`}
                                                 onClick={() => handleSubmitCollectedData()}
                                             >
-                                                <CircleCheckBig className="mr-1" />
+                                                <CircleCheckBig className="mr-1" size={12} />
                                                 Submit</button>
-                                        </div> :
-                                        <>
-                                            <p className="ps-3">{progresRender(item.progress)}</p>
-                                        </>
+                                        </div>
+                                        :
+                                        <div className="ps-3 py-1 border-r border-gray-300">
+                                            <button
+                                                style={{ fontSize: '10px' }}
+                                                className={`h-7 w-3/4 flex justify-center items-center px-1 text-white  border border-gray-300 bg-gray-500  shadow-md hover:shadow-lg transition-shadow duration-300`}
+                                                onClick={() => handleSetMonitoredItemState(item.from, item.id)}
+                                            >
+                                                <BetweenHorizontalStart className="mr-1" size={12} />
+                                                Collect Data</button>
+                                        </div>
                                     }
                                 </div>
                             </div>
@@ -187,14 +218,15 @@ const ProjectMonitoringShow = ({ params }: { params: { monitoringId: string } })
                 return <div className="flex">
                     <div className="w-full flex flex-col">
                         <div className=" bg-gray-300">
-                            <div className="grid grid-cols-7">
+                            <div className="grid grid-cols-8">
                                 <p className="border-r border-gray-400 ps-2 py-2">#</p>
                                 <p className="border-r border-gray-400 ps-2 py-2">Activity Name</p>
                                 <p className="border-r border-gray-400 ps-2 py-2">Input Type</p>
                                 <p className="border-r border-gray-400 ps-2 py-2">Input Name</p>
                                 <p className="border-r border-gray-400 ps-2 py-2">Budget (Tzs)</p>
                                 <p className="border-r border-gray-400 ps-2 py-2">Expense (Tzs)</p>
-                                <p className="ps-2 py-2">Progress</p>
+                                <p className="border-r border-gray-400 ps-2 py-2">Progress</p>
+                                <p className="ps-2 py-2"></p>
                             </div>
                         </div>
                         <div className="" >
@@ -202,35 +234,43 @@ const ProjectMonitoringShow = ({ params }: { params: { monitoringId: string } })
                                 {
                                     payload.data.map((item: any, index: any) =>
                                         <div key={index} className="flex flex-col odd:bg-gray-200" >
-                                            <div className="grid grid-cols-7 w-full text-sm font-light"
+                                            <div className="grid grid-cols-8 w-full text-sm font-light"
                                             >
                                                 <p className="border-r border-gray-300 ps-2 py-1">{index + 1}</p>
                                                 <p className="border-r border-gray-300 ps-2 py-1">{item.activity}</p>
                                                 <p className="border-r border-gray-300 ps-2 py-1">{item.type}</p>
                                                 <p className="border-r border-gray-300 ps-2 py-1">{item.name}</p>
                                                 <p className="border-r border-gray-300 ps-2 py-1">{FormattedMoney({ amount: item.amount, isHideCurrency: true })}</p>
-
-                                                {isCollecting ?
-                                                    <div className="px-3 border-r border-gray-300 py-1 w-full">
-                                                        <input type="text" placeholder="Enter Expense" className="ps-1 h-7 w-full" onChange={(e) => handleFormInputChange(e, item, 'cost')} />
-                                                    </div>
-                                                    :
-                                                    <p className="border-r border-gray-300 ps-2 py-1">{FormattedMoney({ amount: item.occured_cost, isHideCurrency: true })}</p>
+                                                <p className="border-r border-gray-300 ps-2 py-1">{FormattedMoney({ amount: item.occured_cost, isHideCurrency: true })}</p>
+                                                {
+                                                    checkIsMonitoredItem(item.type, item.id) ?
+                                                        <div className="px-3 border-r border-gray-300 py-1 w-full">
+                                                            <input type="text" placeholder="Enter Expense" className="ps-1 h-7 w-full" onChange={(e) => handleFormInputChange(e, item, 'cost')} />
+                                                        </div>
+                                                        :
+                                                        <p className="border-r border-gray-300 ps-2 py-1">{progresRender(item.utilization)}</p>
 
                                                 }
 
-                                                {isCollecting ?
+                                                {checkIsMonitoredItem(item.type, item.id) ?
                                                     <div className="mx-auto py-1 ">
                                                         <button
-                                                            className={`h-7 w-16 flex justify-center items-center px-1 text-white border border-gray-300 bg-gray-500  shadow-md hover:shadow-lg transition-shadow duration-300`}
+                                                            style={{ fontSize: '10px' }}
+                                                            className={`h-7 w-full flex justify-center items-center px-1 text-white border border-gray-300 bg-gray-500  shadow-md hover:shadow-lg transition-shadow duration-300`}
                                                             onClick={() => handleSubmitCollectedData()}
                                                         >
-                                                            <CircleCheckBig className="mr-1" />
+                                                            <CircleCheckBig className="mr-1" size={12} />
                                                             Submit</button>
                                                     </div> :
-                                                    <>
-                                                        <p className="px-3 py-1">{progresRender(item.utilization)}</p>
-                                                    </>
+                                                    <div className="ps-3 py-1 border-r border-gray-300">
+                                                        <button
+                                                            style={{ fontSize: '10px' }}
+                                                            className={`h-7 w-full flex justify-center px-1 items-center text-white  border border-gray-300 bg-gray-500  shadow-md hover:shadow-lg transition-shadow duration-300`}
+                                                            onClick={() => handleSetMonitoredItemState(item.type, item.id)}
+                                                        >
+                                                            <BetweenHorizontalStart className="me-1" size={12} />
+                                                            Collect Expense</button>
+                                                    </div>
                                                 }
                                             </div>
                                         </div>
@@ -340,13 +380,7 @@ const ProjectMonitoringShow = ({ params }: { params: { monitoringId: string } })
                                                             <div className="flex justify-between">
                                                                 <h3 className="p-1 font-semibold">{pay.name}</h3>
                                                                 <div className="flex justify-center gap-3 items-center p-2">
-                                                                    <button
-                                                                        className={`border flex items-center text-sm text-white  border-gray-300 px-2 py-1 ${isCollecting ? 'bg-gray-400' : 'bg-gray-500 '} shadow-md hover:shadow-lg transition-shadow duration-300`}
-                                                                        onClick={() => handleCollectAction()}
-                                                                    >
-                                                                        {isCollecting ? <Activity size={10} className="mr-1" /> : <ClipboardCheck size={15} className="mr-1" />}
-                                                                        {isCollecting ? 'Stop Monitoring ...' : 'Start Monitoring'}
-                                                                    </button>
+
                                                                     <CircularWithValueLabel value={Number(pay.progress)} />
                                                                 </div>
                                                             </div>
