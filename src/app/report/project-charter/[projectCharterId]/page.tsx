f38e311ff896878ac8641b8dd956354f4a486f65
Boniface Mwandobo <breezojr@gmai.com>
2024-08-01 08:31:32 +0300

    charter refactor

--- src/app/report/project-charter/[projectCharterId]/page.tsx
+++ src/app/report/project-charter/[projectCharterId]/page.tsx
@@ -1,14 +1,20 @@
 "use client"
 
 import ProtectedRoute from '@/components/authentication/protected-route'
+import { ReusableButton } from '@/components/button/reusable-button'
+import PageHeader from '@/components/header/page-header'
 import { getValueFromLocalStorage } from '@/utils/actions/local-starage'
-import { get } from '@/utils/api'
+import { baseURL, get } from '@/utils/api'
+import { Download, FileDown } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 
 const ProjectCharterShow = ({ params }: { params: { projectCharterId: string } }) => {
     const id = params.projectCharterId
     const [data, setData] = useState<any>(null)
     const [loading, setLoading] = useState(false)
+    const [isDownloading, setIsDownloading] = useState(false)
+    const [pdfData, setPdfData] = useState<any>(null);
+    const [isloadingGenaratePdf, setIsLoadingGeneratePdf] = useState(false)
     const token = getValueFromLocalStorage('token')
 
     const url = `reports/gantt-chart/show/${id}`
@@ -27,14 +33,91 @@ const ProjectCharterShow = ({ params }: { params: { projectCharterId: string } }
         fetchData()
     }, [url, token])
 
+    const handleClick = async () => {
+        return await generatePdf()
+    }
+
+    const refreshDownloadButton = () => {
+        setIsDownloading(false)
+    }
+
+    const generatePdf = async () => {
+        const strippedToken = token?.substring(1, token.length - 1)
+
+        setIsLoadingGeneratePdf(true);
+        try {
+            const response = await fetch(`${baseURL}/reports/generate_pdf/${id}`, {
+                headers: {
+                    'Authorization': `Bearer ${strippedToken}`, // Include token if authentication is required
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status} ${response.statusText}`);
+            }
+
+            const pdfBlob = await response.blob();
+
+            setPdfData(URL.createObjectURL(pdfBlob));
+            setIsDownloading(true);
+        } catch (error) {
+            console.error('Error in testFetch', error);
+        } finally {
+            setIsLoadingGeneratePdf(false);
+        }
+    };
+
+
+    const ButtonDownloadComponent = () => {
+        return (
+            <>
+                {
+                    isloadingGenaratePdf ?
+                        <p className="text-xs">Generating PDF ...</p>
+                        :
+                        <>
+                            {
+                                isDownloading ?
+                                    <div className="flex gap-3 items-center">
+                                        <p className="text-xs">{`${data.project_name}.pdf`}</p>
+                                        <a className="flex text-xs items-center text-blue-700 shadow px-2 py-1 hover:bg-green-600 hover:text-white hover:px-3  hover:py-1" href={pdfData} download={`${data.project_name}.pdf`} onClick={refreshDownloadButton}>
+                                            <Download className="me-1" size={15} />  Download PDF
+                                        </a>
+                                    </div>
+                                    :
+                                    < div className=''>
+                                        <ReusableButton
+                                            name={'Download'}
+                                            onClick={() => handleClick()}
+                                        >
+                                            <FileDown size={15} />
+                                        </ReusableButton>
+                                    </div>
+                            }
+                        </>
+                }
+            </>
+        )
+    }
+
     return (
         <ProtectedRoute>
             {
                 loading ? <p>Loading...</p>
                     :
                     <div className='flex flex-col'>
-                        <div className='flex flex-col items-center'>
-                            <h3>Project Charter</h3>
+                        <PageHeader
+                            links={[
+                                { name: 'Project Charter', linkTo: '/report/project_charter', permission: '' },
+                                { name: 'Show', linkTo: '/projects/show', permission: '' },
+                            ]}
+                            isShowPage={true}
+                            isDownload={true}
+                            ButtonDownloadComponent={ButtonDownloadComponent}
+                        />
+                        <div className='flex flex-col bg-white p-2 shadow-md text-xs'>
+                            <h3 className='text-sm font-semibold mb-1'>Project Charter</h3>
                             <div className='w-full flex flex-col mb-3'>
                                 <h4>1. General Information</h4>
                                 <div className='ml-12'>
@@ -210,7 +293,7 @@ const ProjectCharterShow = ({ params }: { params: { projectCharterId: string } }
                                     <>
                                         <div className="flex border-l  border-b border-gray-500 font-semibold">
                                             <p className="flex-grow w-48 p-1 border-r border-gray-500">Position</p>
-                                            <p className="flex-grow w-48 p-1 border-r border-gray-500">Position</p>
+                                            <p className="flex-grow w-24 p-1 border-r border-gray-500">Position</p>
                                             <p className="flex-grow w-24 p-1 border-r border-gray-500">Name/Title/Organization</p>
 
                                         </div>
@@ -240,7 +323,7 @@ const ProjectCharterShow = ({ params }: { params: { projectCharterId: string } }
                                         </div>
                                         <div className="flex border-l  border-b border-gray-500 font-semibold">
                                             {/* <p className="flex-shrink-0 w-12 p-1 border-r border-gray-500">2</p> */}
-                                            <p className="flex-grow w-24 p-1 border-r border-gray-500">Name/Title/Organization</p>
+                                            <p className="flex-grow w-48 p-1 border-r border-gray-500">Name/Title/Organization</p>
                                             <p className="flex-grow w-24 p-1 border-r border-gray-500">Name/Title/Organization</p>
 
                                         </div>
@@ -283,14 +366,14 @@ const ProjectCharterShow = ({ params }: { params: { projectCharterId: string } }
                                         </div>
                                         <>
                                             <div className="flex border-l  border-b border-gray-500 font-semibold">
-                                                <p className="flex-shrink-0 w-12 p-1 border-r border-gray-500">Position</p>
-                                                <p className="flex-grow w-48 p-1 border-r border-gray-500">Position</p>
+                                                <p className="flex-grow w-48 p-1 border-r border-gray-500">Name</p>
+                                                <p className="flex-grow w-24 p-1 border-r border-gray-500">Position</p>
                                                 <p className="flex-grow w-24 p-1 border-r border-gray-500">Name/Title/Organization</p>
 
                                             </div>
                                             <div className="flex border-l  border-b border-gray-500 font-semibold">
-                                                <p className="flex-shrink-0 w-12 p-1 border-r border-gray-500">Position</p>
-                                                <p className="flex-grow w-48 p-1 border-r border-gray-500">Position</p>
+                                                <p className="flex-grow w-48 p-1 border-r border-gray-500">Name</p>
+                                                <p className="flex-grow w-24 p-1 border-r border-gray-500">Position</p>
                                                 <p className="flex-grow w-24 p-1 border-r border-gray-500">Name/Title/Organization</p>
 
                                             </div>
@@ -310,14 +393,14 @@ const ProjectCharterShow = ({ params }: { params: { projectCharterId: string } }
                                     </div>
                                     <>
                                         <div className="flex border-l  border-b border-gray-500 font-semibold">
-                                            <p className="flex-shrink-0 w-12 p-1 border-r border-gray-500">Position</p>
+                                            <p className="flex-grow w-48 p-1 border-r border-gray-500">Position/Title</p>
                                             <p className="flex-grow w-48 p-1 border-r border-gray-500">Position</p>
                                             <p className="flex-grow w-24 p-1 border-r border-gray-500">Name/Title/Organization</p>
                                             <p className="flex-grow w-24 p-1 border-r border-gray-500">Name/Title/Organization</p>
 
                                         </div>
                                         <div className="flex border-l  border-b border-gray-500 font-semibold">
-                                            <p className="flex-shrink-0 w-12 p-1 border-r border-gray-500">Position</p>
+                                            <p className="flex-grow w-48 p-1 border-r border-gray-500">Position/Title</p>
                                             <p className="flex-grow w-48 p-1 border-r border-gray-500">Position</p>
                                             <p className="flex-grow w-24 p-1 border-r border-gray-500">Name/Title/Organization</p>
                                             <p className="flex-grow w-24 p-1 border-r border-gray-500">Name/Title/Organization</p>
