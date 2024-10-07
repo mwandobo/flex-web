27545a50b408d8aea2e0b7dbd9f61dc61adb3e4e
Boniface Mwandobo <breezojr@gmai.com>
2024-10-07 22:25:43 +0300

    introduction to bid pr

--- src/app/procurement/bid-comparison/bid-comparison-view.tsx
+++ src/app/procurement/bid-comparison/bid-comparison-view.tsx
@@ -2,24 +2,22 @@
 
 import ProtectedRoute from "@/components/authentication/protected-route";
 import MuiCardComponent from "@/components/card/mui-card.component";
-import ViewCardComponent from "@/components/card/view.card.component";
-import { getValueFromLocalStorage } from "@/utils/actions/local-starage";
-import { get } from "@/utils/api";
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import {getValueFromLocalStorage} from "@/utils/actions/local-starage";
+import {get, post, remove} from "@/utils/api";
+import React, {useEffect, useState} from "react";
+import {useRouter} from "next/navigation";
 import {useGlobalContextHook} from "@/hooks/useGlobalContextHook";
 import PageHeader from "@/components/header/page-header-v1";
-import RequisitionRequestItem from "@/app/procurement/requisition-requests/requisition-request-items";
-import {useCrudOperator} from "@/hooks/crud/crud-operator";
+import CircleCheckbox from "@/components/inputs/check-circle.component";
 import {ReusableButton} from "@/components/button/reusable-button";
-import {FileOutput} from "lucide-react";
-import RfqItems from "@/app/procurement/rfq/rfq-items";
-import QuotationItems from "@/app/procurement/quotation/quotation-items";
+import {CheckCircle} from "lucide-react";
 
 const BidComparisonView = () => {
 
     const [data, setData] = useState<any>([])
+    const [rfq, setRfq] = useState<any>({})
     const [loading, setLoading] = useState(false)
+    const [refresh, setRefresh] = useState(false)
     const router = useRouter()
     const token = getValueFromLocalStorage('token')
 
@@ -27,101 +25,100 @@ const BidComparisonView = () => {
     const {selectedSubSidebarItem: selected, viewedItem} = state;
     const {id, from: viewFrom} = viewedItem;
 
-    const url = `quotations/${id}`
+    const url = `bid-comparison/${id}`
     const navigateToLogin = () => {
         return router.push('/login')
     }
 
-    const formInputs = [
-        {
-            name: 'payment_method',
-            type: 'text',
-            label: 'Payment Method',
-            value: '',
-            required: true,
-            isError: false,
-            errorMessage: ''
-        },
-        {
-            name: 'evaluation_method',
-            type: 'text',
-            label: 'Evaluation Method',
-            value: '',
-            required: true,
-            isError: false,
-            errorMessage: ''
-        },
-        {
-            name: 'decision_timeline',
-            type: 'text',
-            label: 'Decision Timeline',
-            value: '',
-            required: true,
-            isError: false,
-            errorMessage: ''
-        },
-        {
-            name: 'submission_requirement',
-            type: 'text',
-            label: 'Submission Requirement',
-            value: '',
-            required: true,
-            isError: false,
-            errorMessage: ''
-        },
-        {
-            name: 'delivery_time',
-            type: 'text',
-            label: 'Deliver Time',
-            value: '',
-            required: true,
-            isError: false,
-            errorMessage: ''
-        },
-        {
-            name: 'terms_and_conditions',
-            type: 'textArea',
-            label: 'Terms and Conditions',
-            value: '',
-            required: true,
-            isError: false,
-            errorMessage: ''
-        },
-    ]
+    const [selectedCards, setSelectedCards] = useState<any[]>([]);
 
-    const {
-        handleClick,
-        createdForm,
-        isStateChanged
-    } = useCrudOperator({
-        formInputData: formInputs,
-        incomingUrl: `rfq/${id}/create-rfq`,
-        incomingModalTitle: "Request For Quotation",
-        viewUrl:"",
-        state_properties:[],
-        from:'rfq',
-        isApiV2:true
-    })
+    const handleCheckboxChange = (itemId: number, supplier: any) => {
+        setSelectedCards((prevSelectedCards) => {
+            return prevSelectedCards.map((selected) =>
+                selected.item_id === itemId ? {
+                    ...selected, winner: {
+                        supplier_id: supplier.id,
+                        quotation_id: supplier.quotation_id,
+                        price: supplier.price,
+                        quantity: supplier.quantity
+                    }
+                } : selected
+            );
+        });
+    };
+
+    const handleSubmit = async () => {
+        const body = {
+            winners: selectedCards
+        }
+        const response = await post(`bid-comparison/${id}/winners`, body, token)
+
+        if (response.status === 200) {
+            setRefresh(!refresh)
+        }
+    };
+
+    const handleRefreshWinner = async () => {
+        const response = await remove(`bid-comparison/${id}/remove-winners`, token)
+
+        if (response.status === 200) {
+            setRefresh(!refresh)
+        }
+    };
+
+    const handleCreatePurchaseRequest = async () => {
+        const response = await remove(`bid-comparison/${id}/remove-winners`, token)
+
+        if (response.status === 200) {
+            setRefresh(!refresh)
+        }
+    };
+
+    const isSelected = (itemId: number, supplier: any) => {
+        let status = false
+        const selection = selectedCards.find((item) => item.item_id === itemId);
+
+        if (selection?.winner.quotation_id && supplier) {
+            status = selection?.winner.quotation_id === supplier.quotation_id
+        }
+        return status
+    };
+
+    const fetchData = async () => {
+        try {
+            setLoading(true)
+            const res = await get(url, token)
+
+            if (data && res.status === 200) {
+                const result = res.data.data
+                setData(result)
+                setRfq(res.data.rfq)
+
+                const bufBody = result.map(item => ({
+                    item_id: item.id,
+                    winner:
+                        {
+                            supplier_id: null,
+                            quotation_id: null,
+                            price: null,
+                            quantity: null
+                        },
+                }))
+
+                setSelectedCards(bufBody)
+                setLoading(false)
+            }
+
+        } catch (error: any) {
+            if (error?.code === "ERR_NETWORK") {
+                navigateToLogin()
+            }
+        }
+    };
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setLoading(true)
-                const res = await get(url, token)
-
-                if (data && res.status === 200) {
-                    setData(res.data.data)
-                    setLoading(false)
-                }
-
-            } catch (error: any) {
-                if (error?.code === "ERR_NETWORK") {
-                    navigateToLogin()
-                }
-            }
-        };
         fetchData()
-    }, [])
+    }, [refresh])
 
     return (
 
@@ -131,36 +128,75 @@ const BidComparisonView = () => {
                     :
                     <>
                         <PageHeader
-                           title={'Quotation View'}
-                           isShowBackButton={true}
+                            title={'Bid Comparison View'}
+                            isShowBackButton={true}
                         />
                         <MuiCardComponent>
-                            <div className="mb-3">
-                                <ViewCardComponent
-                                    data={[
-                                        {label: 'Quotation Code', value: data?.formatted_code},
-                                        {label: 'Supplier', value: data?.supplier_name},
-                                        {label: 'Request for Quotation Code', value: data?.rfq_name},
-                                        {label: 'Payment Method', value: data?.payment_method},
-                                        {label: 'Evaluation Method', value: data?.evaluation_method},
-                                        {label: 'Decision Timeline', value: data?.decision_timeline},
-                                        {label: 'Submission Requirement', value: data?.submission_requirement},
-                                        {label: 'Delivery Time', value: data?.delivery_time},
-                                        {label: 'Terms and Conditions', value: data?.terms_and_conditions},
+                            {data.map((payloadItem, index) => (
+                                <div key={index} className={'grid grid-cols-7 border-t border-gray-300'}>
+                                    <div className={'col-span-2 text-xs flex flex-col justify-center'}>
+                                        <div className="grid grid-cols-2">
+                                            <p className="col-span-1 text-right mr-2">Item Name:</p>
+                                            <p className="col-span-1 font-semibold text-left">{payloadItem.item}</p>
+                                        </div>
+                                        <div className="grid grid-cols-2">
+                                            <p className="col-span-1 text-right mr-2">Item Price:</p>
+                                            <p className="col-span-1 font-semibold text-left">{payloadItem.price}</p>
+                                        </div>
+                                    </div>
 
-                                    ]}
-                                    titleA={`Quotation`}
-                                    titleB={` ${data?.formatted_code} `}
-                                />
-                            </div>
-                            <hr className="bg-gray-100"/>
-                            <div className={'mt-2'}>
-                                <QuotationItems quotation_id={id}/>
-                            </div>
+                                    <div className="col-span-5 grid grid-cols-2 gap-4 p-2">
+                                        {payloadItem.suppliers.map((supplier, index) => (
+                                            <div
+                                                key={index}
+                                                onClick={() => handleCheckboxChange(payloadItem.id, supplier)}
+                                                className={`p-4 mb-1 mt-1 mr-1 border border-gray-200 shadow-md text-xs hover:cursor-pointer 
+                                                              transform transition-all duration-300 ease-in-out 
+                                                              ${supplier?.winner === "winned" ? 'bg-gray-200  hover:bg-gray-300' :
+                                                               isSelected(payloadItem.id, supplier) ?
+                                                               'bg-gray-200 shadow-lg scale-105 hover:bg-gray-300' : 
+                                                                   `bg-white  ${rfq?.status ==="pending" && 'hover:scale-105 hover:bg-gray-200'}   `} 
+                                                              hover:shadow-lg `}
+                                            >
+                                                <div>
+                                                    {
+                                                        rfq?.status === "pending" && <CircleCheckbox
+                                                        checked={isSelected(payloadItem.id, supplier)}
+                                                    />
+                                                    }
 
+                                                </div>
+                                                <div className="grid grid-cols-2">
+                                                    <p className="col-span-1 text-right mr-3">Supplier Name:</p>
+                                                    <p className="col-span-1 font-semibold text-left">{supplier.supplierName}</p>
+                                                </div>
+                                                <div className="grid grid-cols-2">
+                                                    <p className="col-span-1 text-right mr-3">Quotation Price:</p>
+                                                    <p className="col-span-1 font-semibold text-left">{supplier.price}</p>
+                                                </div>
+                                                <div className="grid grid-cols-2">
+                                                    <p className="col-span-1 text-right mr-3">Payment Method:</p>
+                                                    <p className="col-span-1 font-semibold text-left">{supplier.payment_method}</p>
+                                                </div>
+                                                <div className="grid grid-cols-2">
+                                                    <p className="col-span-1 text-right mr-3">Delivery Time:</p>
+                                                    <p className="col-span-1 font-semibold text-left">{supplier.delivery_time}</p>
+                                                </div>
+                                                <div className="grid grid-cols-2">
+                                                    <p className="col-span-1 text-right mr-3">Terms and Conditions:</p>
+                                                    <p className="col-span-1 font-semibold text-left">{supplier.terms_and_conditions}</p>
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
+                            <div className={'flex justify-end gap-1'}>
+                                {rfq?.status === "pending" && <ReusableButton name={'Submit'} onClick={handleSubmit}> <CheckCircle size={10} /> </ReusableButton>}
+                                {rfq?.status === "bid-comparison" && <ReusableButton name={'Re Choose Winner'} onClick={handleRefreshWinner}>  <CheckCircle size={10} /> </ReusableButton>}
+                                {rfq?.status === "bid-comparison" && <ReusableButton name={'Create Purchase Request'} onClick={handleCreatePurchaseRequest}> <CheckCircle size={10} /> </ReusableButton>}
+                            </div>
                         </MuiCardComponent>
-                        {createdForm()}
-
                     </>
             }
         </ProtectedRoute>
--- src/app/procurement/page.tsx
+++ src/app/procurement/page.tsx
@@ -9,27 +9,33 @@ import Quotation from "@/app/procurement/quotation/quotation";
 import QuotationView from "@/app/procurement/quotation/quotation-view";
 import BidComparison from "@/app/procurement/bid-comparison/bid-comparison";
 import BidComparisonView from "@/app/procurement/bid-comparison/bid-comparison-view";
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 import {getValueFromLocalStorage} from "@/utils/actions/local-starage";
 import {useGlobalContextHook} from "@/hooks/useGlobalContextHook";
 
 const ProcurementItems = [
-    { name: 'requisition-requests', title: 'Requisition Requests', item: <RequisitionRequest />, itemView: <RequisitionRequestView /> },
-    { name: 'request-for-quotation', title: 'Requests For Quotations', item: <Rfq />, itemView: <RfqView /> },
-    { name: 'quotation', title: 'Quotations', item: <Quotation />, itemView: <QuotationView /> },
-    { name: 'bid-comparison', title: 'Bid Comparison', item: <BidComparison />, itemView: <BidComparisonView /> },
+    {
+        name: 'requisition-requests',
+        title: 'Requisition Requests',
+        item: <RequisitionRequest/>,
+        itemView: <RequisitionRequestView/>
+    },
+    {name: 'request-for-quotation', title: 'Requests For Quotations', item: <Rfq/>, itemView: <RfqView/>},
+    {name: 'quotation', title: 'Quotations', item: <Quotation/>, itemView: <QuotationView/>},
+    {name: 'bid-comparison', title: 'Bid Comparison', item: <BidComparison/>, itemView: <BidComparisonView/>},
 ];
 
 function InventoryPage() {
-    const { state, dispatch } = useGlobalContextHook();
+    const {state, dispatch} = useGlobalContextHook();
     useEffect(() => {
         const selectedItem = getValueFromLocalStorage('selected_sub_sidebar_item')
-        const sideViewPayload= JSON.parse(getValueFromLocalStorage('sub_view_item'))
-        dispatch({ type: "SET_SUB_SIDEBAR_ITEM", payload: selectedItem });
-        dispatch({type: "SET_SUB_VIEW_ITEM", payload: sideViewPayload })
+        const sideViewPayload = JSON.parse(getValueFromLocalStorage('sub_view_item'))
+        dispatch({type: "SET_SUB_SIDEBAR_ITEM", payload: selectedItem});
+        dispatch({type: "SET_SUB_VIEW_ITEM", payload: sideViewPayload})
     }, []);
 
-    return <InternalMenuSkeletonComponent pageItems={ProcurementItems} title="Procurement Management"  subtitle='Procurement Items'/>;
+    return <InternalMenuSkeletonComponent pageItems={ProcurementItems} title="Procurement Management"
+                                          subtitle='Procurement Items'/>;
 }
 
 export default InventoryPage;
