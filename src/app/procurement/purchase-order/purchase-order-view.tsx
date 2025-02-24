bd115d719292f0c9ebcc52f9fb2113dc9434e622
Boniface Mwandobo <breezojr@gmail.com>
2025-02-24 17:19:03 +0300

    purchase order approval refactor

--- src/app/procurement/purchase-order/purchase-order-view.tsx
+++ src/app/procurement/purchase-order/purchase-order-view.tsx
@@ -9,19 +9,13 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import {useGlobalContextHook} from "@/hooks/useGlobalContextHook";
 import PageHeader from "@/components/header/page-header-v1";
-import RequisitionRequestItem from "@/app/procurement/requisition-requests/requisition-request-items";
-import {useCrudOperator} from "@/hooks/crud/crud-operator";
 import {ReusableButton} from "@/components/button/reusable-button";
-import {FileOutput, RotateCcw} from "lucide-react";
-import RfqItems from "@/app/procurement/rfq/rfq-items";
-import QuotationItems from "@/app/procurement/quotation/quotation-items";
+import {CheckCircle2,} from "lucide-react";
 import PurchaseOrderItems from "@/app/procurement/purchase-order/purchase-order-items";
 import moneyFormater from "@/components/moneyFormater";
-import {ITEM_APPROVAL_SLUG, PURCHASE_ORDER_APPROVAL_SLUG} from "@/utils/constant";
-import {useApprovalHook} from "@/hooks/useApprove";
-import SlideOver from "@/components/slide-over/slide-over.component";
-import TreeList from "@/components/list/tree-list.component";
+import { PURCHASE_ORDER_APPROVAL_SLUG} from "@/utils/constant";
 import {showConfirmationModal} from "@/utils/showAlertDialog";
+import {useApprovalsAndButtonsHook} from "@/hooks/useApprovalAndButtons.hook";
 
 const PurchaseOrderView = () => {
 
@@ -40,20 +34,13 @@ const PurchaseOrderView = () => {
         return router.push('/login')
     }
 
-    const approval_url = `approval/approved-items/by-item?from=${PURCHASE_ORDER_APPROVAL_SLUG}&&from_id=${id}`
-
     const {
-        isNeedApprove,
-        isLastLevel,
-        latestApproveStatus,
-        approvalButtonsWrapper,
-    } = useApprovalHook({
+        approvalsAndButtonsWrapper,
+    } = useApprovalsAndButtonsHook({
         approval_slug: PURCHASE_ORDER_APPROVAL_SLUG,
         from: PURCHASE_ORDER_APPROVAL_SLUG,
         from_id: id
     })
-
-    const approveStatus = () => (!isNeedApprove || (isLastLevel && latestApproveStatus === 'approve'))
 
     const onSave = async () => {
         try {
@@ -66,7 +53,7 @@ const PurchaseOrderView = () => {
         }
     };
 
-    const handleSubmit = (data: any) => {
+    const handleSubmit = () => {
         showConfirmationModal({
             title: 'Are You Sure?',
             text: `Are You Sure You Want To Submit Purchase Order Code: ${data.formatted_code}?`,
@@ -95,8 +82,31 @@ const PurchaseOrderView = () => {
         fetchData()
     }, [refresh])
 
-    return (
 
+    const buttonsBody = () => {
+        return <>
+            {data?.status === 'pending' &&
+
+
+                    <ReusableButton
+                        name={'Submit Purchase Order'}
+                        onClick={() => handleSubmit()}
+                        rounded={'md'}
+                        padding={'p-3'}
+                        shadow={'shadow-md'}
+                        bg_color={'bg-gray-50'}
+                        hover={'hover:bg-gray-200 hover:border-gray-400'}
+                        hover_text={'hover:text-gray-900 hover:font-semibold'}
+                        border={'border border-gray-300'}
+                        text_color={'text-gray-700'}
+                    >
+                        <CheckCircle2 size={13}/>
+                    </ReusableButton>
+            }
+        </>
+    }
+
+    return (
         <ProtectedRoute>
             {
                 loading ? <p>Loading...</p>
@@ -129,34 +139,13 @@ const PurchaseOrderView = () => {
                                     ]}
                                     titleA={`Purchase Request`}
                                     titleB={` ${data?.formatted_code} `}
+                                    OptionalElement={approvalsAndButtonsWrapper({buttonBody: buttonsBody()})}
                                 />
-                                <div className={'flex justify-between mt-2'}>
-                                    <>
-                                        {approvalButtonsWrapper()}
-                                    </>
-                                    <SlideOver
-                                        showButton={isNeedApprove}
-                                        title="Approval Trail">
-                                        <TreeList
-                                            url={approval_url}
-                                        />
-                                    </SlideOver>
-                                </div>
                             </div>
                             <hr className="bg-gray-100"/>
                             <div className={'mt-2'}>
                                 <PurchaseOrderItems purchase_order_id={id}/>
                             </div>
-                            {approveStatus() && data?.status === 'pending' &&
-                                <div className={'flex justify-end gap-2'}>
-                                    <ReusableButton
-                                        name={'Submit Purchase Order'}
-                                        onClick={() => handleSubmit(data)}
-                                    >
-                                        <FileOutput size={12}/>
-                                    </ReusableButton>
-                                </div>
-                            }
                         </MuiCardComponent>
                     </>
             }
