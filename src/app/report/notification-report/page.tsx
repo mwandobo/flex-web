991271e26c50e209fecca4adaaf32c1da2ac583e
Boniface Mwandobo <breezojr@gmail.com>
2025-03-30 19:52:47 +0300

    notification report filter refactor

--- src/app/report/notification-report/page.tsx
+++ src/app/report/notification-report/page.tsx
@@ -8,6 +8,9 @@ import {get} from "@/utils/api";
 import {getValueFromLocalStorage} from "@/utils/actions/local-starage";
 import CustomTable from "@/components/tables/flexible-normal-table";
 import moneyFormater from "@/components/moneyFormater";
+import {useGlobalContextHook} from "@/hooks/useGlobalContextHook";
+import {createUrlWithFilters} from "@/utils/report-filter.helper";
+import ReportFilterComponent from "@/components/report-filter.component";
 
 const columns = [
     {header: 'Sender Name', accessor: 'user_name'},
@@ -22,14 +25,18 @@ function NotificationReport() {
     const [data, setData] = useState<any>([])
     const [metadata, setMetaData] = useState<any>()
     const [loading, setLoading] = useState(false)
-    const token = getValueFromLocalStorage('token')
     const url = 'report/notification'
+
+    const filter_key = 'notification-report'
+    const {state}  = useGlobalContextHook()
+    const filters = state.filterBody;
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 setLoading(true)
-                const res = await get(url, token)
+                const final_url = createUrlWithFilters(url, filter_key)
+                const res = await get(final_url )
 
                 if ( res.status === 200) {
                     setData(res.data.data.data)
@@ -44,10 +51,14 @@ function NotificationReport() {
             }
         };
         fetchData()
-    }, [])
+    }, [filters])
 
     const pageRender = () =>{
         return <div className={'mt-2'}>
+            <ReportFilterComponent
+                from={filter_key}
+                isApprovalFilter={false}
+            />
             <CustomTable
                 columns={columns}
                 data={data}
@@ -66,6 +77,7 @@ function NotificationReport() {
                             subHeader={'Notification Report'}
                             isShowPage={true}
                             isDownload={true}
+                            filter={filter_key}
                             ButtonDownloadComponent={
                                 <GeneratePdf
                                     content={pageRender()}
